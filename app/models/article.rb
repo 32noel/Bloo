cfaa0ca4b62821295e512af7af5020e302a6af0e
32noel <czferfp@gmail.com>
2023-08-07 15:07:28 -0500

    Se corrige la creacion de comentariosno funcionaba

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,10 @@
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments, dependent: :destroy
+  has_many :comments
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-
-
 
   VALID_STATUSES = ['public', 'private', 'archived']
 
